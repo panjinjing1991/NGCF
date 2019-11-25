7f03e22848df092ed21801607ece5af5e5246355
leelew <sysulewlee1@gmail.com>
2019-11-25 09:24:38 +0800

    Li Lu

--- NGCF/causality_test.m
+++ NGCF/causality_test.m
@@ -1,14 +1,37 @@
-function pass = causality_test(X,Y)
-% use random forest to apply nonlinear granger causality test.
+% granger causality test[1] apply for X as predict variables, Y as target
+% variables to find whether X granger cause Y.
 
-const = ones(numel(Y),1);
-R2_full = models([X,const],Y,'origin',3);
-R2_baseline = models(const,Y,'origin',3);
+% X must as lagged time series, full model is Y(t) = X(t-1)+...X(t-m)+c, and
+% baseline model is Y(t) = c. Thus contrast the performance of two regression 
+% above could see the impact of X(t-m). 
 
-if R2_baseline < R2_full
-    pass = 1;
-else
-    pass = 0;
+% Example: 
+% 1. linear granger causality test
+%    pass = causality_test(X,Y,1);
+% 2. nonlinear granger causality test
+%    pass = causality_test(X,Y,2);
+
+% Referrence:
+% [1]:
+
+% attention: X,Y must pass the station series test,such as ADF test.
+% Copyright(c): Li Lu, 2019
+
+function pass = causality_test(X,Y,type)
+
+if type==1
+    'Need improved'
+elseif type==2
+    % use random forest to apply nonlinear granger causality test.
+    const = ones(numel(Y),1);
+    R2_full = models([X,const],Y,'origin',3);
+    R2_baseline = models(const,Y,'origin',3);
+
+    if R2_baseline < R2_full
+        pass = 1;
+    else
+        pass = 0;
+    end
 end
 
 end
--- NGCF/quatify.m
+++ NGCF/quatify.m
@@ -1,30 +1,19 @@
+% This quaitfy method is from Tuttle and Savicci,2016[1].
+% Their method derived from three steps:
+% (1) regression Y = beta*X+c, and got p value.
+% attention: this part remove the bootstrap method raised by [1] to save time.
+% (2) Use GLM method to seperate dry days and wet days, if anomaly of target day
+% is larger than seasonaly anomaly, set as wet day, vice versa.
+% (3) Use mean of prediction of full model and baseline model(full model set 
+% coefficient of predict variables as 0) to get the qualify value of X-Y
+% feedback.
+
 function [impact,p_value] = quatify(X,Y)
 %
 %
 %
 %
 %
-% if x residual close to 0,corresponding offset value close to +-inf.
-% thus this phenomenen should be exclude.
-offsetR = norminv(X);
-offsetR(find(offsetR<-1000 | offsetR>1000)) = NaN; 
-%
-const = ones(numel(Y),1);    %constant term in regression
-% fit
-try   
-    [b,~,stats] = glmfit([const,X],Y,'binomial',...
-                         'link','probit',...
-                         'estdisp','on',...
-                         'constant','off',...
-                         'offset',offsetR);    
-    dispsave = stats.sfit(end);    %dispersion
-    p_value = stats.p(2);
-catch WRONG   
-    disp('error in unrestrict, uncorrected offset model')
-    dispsave = NaN;
-    p_value = NaN;
-    b(2) = NaN;
-end
 % qualify
 if ~isnan(b(2))
     %
@@ -59,6 +48,32 @@ if ~isnan(b(2))
 else
     p_value = NaN;
     impact = nan(2,1);    
+end
+end
+
+function [b,p_value,disp] = reg_rf2glm(X,Y)
+% function relate random forest regression with glm regression. 
+%
+%
+% 
+[~,predict,~,~] = RF(X,Y);
+offsetR = check_rf2glm(predict);
+%
+const = ones(numel(Y),1);    %constant term in regression
+% fit
+try   
+    [b,~,stats] = glmfit([const,X],Y,'binomial',...
+                         'link','probit',...
+                         'estdisp','on',...
+                         'constant','off',...
+                         'offset',offsetR);    
+    disp = stats.sfit(end);    %dispersion
+    p_value = stats.p(2);
+catch WRONG   
+    disp('error in unrestrict, uncorrected offset model')
+    disp = NaN;
+    p_value = NaN;
+    b(2) = NaN;
 end
 end
 
@@ -120,5 +135,18 @@ clim_ = [const,season_terms(:,KK)]*b;
 % subtract mean S climatology from S
 season_anomaly = X - clim_;
 
-
 end
+
+function X = check_rf2glm(X)
+
+% random forest changed to glm method, need to set down the offsetR
+% parameters:
+% __________
+% X: ***X must be the prediction array of random foreset regression
+%
+% if x residual close to 0,corresponding offset value close to +-inf.
+% thus this phenomenen should be exclude.
+offsetR = norminv(X);
+offsetR(find(offsetR<-1000 | offsetR>1000)) = NaN;
+
+end 
